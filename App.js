05d2cecd769eacd31ff1f42c3eae42afd5ec2afb
Matheus Veillard <71525220+veillardx@users.noreply.github.com>
2025-04-02 00:22:10 -0300

    novo
    novo

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-
 import React from "react";
+
 function App() {
-  return <h1>Simulador de Cubagem</h1>;
+  return (
+    <div>
+      <h1>Simulador de Cubagem</h1>
+      <p>App base carregado com sucesso!</p>
+    </div>
+  );
 }
+
 export default App;
