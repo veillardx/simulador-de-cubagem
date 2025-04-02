85e441c1354b125d16d6ba1819b7e2db8cbe4563
Matheus Veillard <71525220+veillardx@users.noreply.github.com>
2025-04-02 00:30:34 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Simulador de Cubagem</h1>
+      <p>Aplicação carregada com sucesso.</p>
+    </div>
+  );
+}
+export default App;
